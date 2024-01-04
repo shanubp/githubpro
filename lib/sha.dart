e1a78adc4740b7fedf2f3acbbb36b45e4b36e733
user <aslamtt553@gmail.com>
2024-01-04 16:18:04 +0530

    new

---
+++ lib/sha.dart
@@ -0,0 +1,13 @@
+class New extends StatefulWidget {
+  const New({Key? key}) : super(key: key);
+
+  @override
+  State<New> createState() => _NewState();
+}
+
+class _NewState extends State<New> {
+  @override
+  Widget build(BuildContext context) {
+    return Sc;
+  }
+}
